b0df330a07d1a18f0951c07cb2316f52d1f47c8b
Elder Oliveira <elderoliveiracastro@gmail.com>
2024-10-28 18:17:55 -0300

    Configuring the database

---
+++ api/Models/Comment.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace api.Models
+{
+    public class Comment {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Content { get; set; } = string.Empty;
+        
+        public DateTime CreatedOn { get; set; } = DateTime.Now;  
+
+        public int? StockID { get; set; }
+
+        public Stock? Stock { get; set; }
+
+
+
+    }
+}
