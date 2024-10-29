b2a1e05409e0c208c8843f576b9569a99d9c23f2
Elder Oliveira <elderoliveiracastro@gmail.com>
2024-10-29 17:12:32 -0300

    First post method

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a38238e492e0b9101bbc08b7ddca6118afb6f06d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84b80d84dd4f7805b2275b3e82a520b906f5f74f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
